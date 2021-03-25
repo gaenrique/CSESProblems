3a37b9d65fedab2cdda8bf5e23ead94a459e9eca
Enrique Gonzalez Aretos <gonzalezaretos@gmail.com>
2021-03-26 00:55:58 +1000

    Permutations solution

---
+++ IntroProblems/permutations.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n == 1)
+    {
+        cout << n << endl;
+        return 0;
+    }
+    if (n < 4)
+    {
+        cout << "NO SOLUTION" << endl;
+        return 0;
+    }
+    for (int i = 2; i <= n; i += 2)
+    {
+        cout << i << " ";
+    }
+    for (int i = 1; i <= n; i += 2)
+    {
+        cout << i << " ";
+    }
+    return 0;
+}
